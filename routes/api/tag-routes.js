ac5347daa663140f850d539bd9a7a64063b2ce09
May <MayyFaucher@gmail.com>
2021-10-28 16:24:55 -0700

    everything works perfectly, completed all of tag-routes @ router.delete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,6 +62,7 @@ router.put('/:id', async (req, res) => {
       res.status(404).json({ message: "No tag found with that id" });
       return;
     }
+    // res.status(200).json(tagUpdate);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -69,6 +70,19 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+    // res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
