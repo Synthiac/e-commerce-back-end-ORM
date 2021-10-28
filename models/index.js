e38f3b5d6d90bf854f34d828d90d4b068db7085b
May <MayyFaucher@gmail.com>
2021-10-28 14:52:49 -0700

    fixed delete route on category-seeds.js

--- models/index.js
+++ models/index.js
@@ -7,23 +7,21 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-  // onDelete: "SET NULL"
+  onDelete: "CASCADE"
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE"
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-  },
+  through: { model: ProductTag, foreignKey: "product_id" },
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    foreignKey: "tag_id"
   },
 })
 module.exports = { Product, Category, Tag, ProductTag };
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     const tagData = await Tag.findAll({
       include: [{ model: Product, through: ProductTag }]
     });
-
+    console.log(tagData)
     res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
